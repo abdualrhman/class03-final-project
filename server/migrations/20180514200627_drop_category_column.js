68643db741e8d699cc81a509241dd45da16d1cda
Moaldahoul <ma811forever@gmail.com>
2018-05-20 15:16:38 +0200

    Removed unnecessary migrations.

--- server/migrations/20180514200627_drop_category_column.js
+++
@@ -1,9 +0,0 @@
-exports.up = function(knex, Promise) {
-  return   knex.schema.table('items', function (table) {
-  table.dropColumn('category');
-  })
-};
-
-exports.down = function(knex, Promise) {
-
-};
