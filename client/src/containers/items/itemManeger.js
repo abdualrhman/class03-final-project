4f53888ab308ab5cee091833ff7d324226b73fc3
Abdulrhman Ramadan <abdalrhman07094h@gmail.com>
2018-05-28 18:59:05 +0200

    change the input handler functions

--- client/src/containers/items/itemManeger.js
+++ client/src/containers/items/itemManeger.js
@@ -16,57 +16,15 @@ import CategoryManeger from '../categories.js';
       description : '',
     }
     //binding the functions
-    this.categoryFunc =this.categoryFunc.bind(this)
-    this.difficultyFunc =this.difficultyFunc.bind(this)
-    this.linkFunc=this.linkFunc.bind(this)
     this.submitFunc=this.submitFunc.bind(this)
-    this.titleFunc=this.titleFunc.bind(this)
-    this.descriptionFunc=this.descriptionFunc.bind(this)
-    this.typeFunc=this.typeFunc.bind(this)
+    this.changeHandler=this.changeHandler.bind(this)
   }
-  // this function changes the states categoty's value
-  categoryFunc(event){
+  // this function the forms input changes
+  changeHandler(event){
+    const {name, value}=event.target;
     this.setState({
-      category : event.target.value
-    })
-    console.log('category: ',this.state.category)
-  }
-  // this function changes the states fifficulty's value
-  difficultyFunc(event){
-    this.setState({
-      difficultyValue : event.target.value
-    })
-    console.log('difficulty: ',this.state.difficultyValue)
-  }
-  // this function changes the states type's value
-  typeFunc(event){
-    console.log(event.target.name)
-    this.setState({
-      type : event.target.value
-    })
-    console.log('difficulty: ',this.state.type)
-  }
-  // this function changes the states link's value
-
-  linkFunc(event){
-    this.setState({
-      link : event.target.value
-    })
-  }
-  // this function changes the states title's value
-
-  titleFunc(event){
-    this.setState({
-      title : event.target.value
-    })
-  }
-  // this function changes the states discreption's value
-
-  descriptionFunc(event){
-    console.log(event.target.name)
-    this.setState({
-      description : event.target.value
-    })
+      [name] : value
+    },()=>{console.log([name]+' :'+value)})
   }
   // this function submits value in database
   submitFunc(event){
@@ -107,12 +65,12 @@ import CategoryManeger from '../categories.js';
         <div>
           <ItemForm
               submitHandler={this.submitFunc}
-              categoryHandler={this.categoryFunc}
-              difficultyHandler={this.difficultyFunc}
-              linkHandler={this.linkFunc}
-              titleHandler={this.titleFunc}
-              descriptionHandler={this.descriptionFunc}
-              typeHandler={this.typeFunc}
+              categoryHandler={this.changeHandler}
+              difficultyHandler={this.changeHandler}
+              linkHandler={this.changeHandler}
+              titleHandler={this.changeHandler}
+              descriptionHandler={this.changeHandler}
+              typeHandler={this.changeHandler}
           />
         </div>
       </div>
