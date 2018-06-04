4ba9dc9e13ca511450248d8fcc432fd8e3b6898b
Abdulrhman Ramadan <abdalrhman07094h@gmail.com>
2018-06-04 10:50:49 +0200

    added a config file

--- client/src/containers/App/Navigation.js
+++ client/src/containers/App/Navigation.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link,NavLink } from "react-router-dom";
+import '../../styles/index.css';
 
 const Navigation = () =>{
     return(
         <div>
-        <nav className='navbar navbar-inverse bg-inverse mb-4'>
-            <Link to='/' className="navbar-brand"> HackYourFuture </Link>
-        </nav>
+            <span >
+                <nav className='navbar-inverse bg-inverse mb-4'> {/* navbar */}
+                    <Link to='/' className="navbar-brand"> HackYourFuture</Link>
+                <span style={{marginLeft : '50%'}}>
+                    <button style={{backgroundColor: 'rgba(225,225,225,0.1)', margin:'10px'}} className='btn btn-navBar'>< NavLink to='/categories' > Categories</ NavLink></button>
+                    <button style={{backgroundColor: 'rgba(225,225,225,0.1)'}} className='btn btn-navBar'>< NavLink to='/' > Add Content</ NavLink></button>
 
+                </span>
+                </nav>
+            </span>
         </div>
     );
 };
