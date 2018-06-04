8b92c23c529f47a4982775a4e22ec7ac4a15630e
Abdulrhman Ramadan <abdalrhman07094h@gmail.com>
2018-06-04 17:29:34 +0200

    deleted the navigation file

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './containers/App/App';
-import Navigation from './containers/App/Navigation';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
     <div>
-      <Navigation />
       <App/>
     </div>
   </BrowserRouter>
