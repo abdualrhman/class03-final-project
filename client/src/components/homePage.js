2f6929b5eb6ce11843814a09ca340c93e52db70a
Abdulrhman Ramadan <abdalrhman07094h@gmail.com>
2018-06-05 15:26:30 +0200

    added the home page

---
+++ client/src/components/homePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import '../styles/index.css';
+
+
+
+ export default class HomePage extends Component {
+
+  render() {
+    return (
+      <div>
+        <img className='image' src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-0.3.5&s=98f0701e92fcb50e8f53e6bb5b4c112e&auto=format&fit=crop&w=1500&q=80"
+        />
+      </div>
+    );
+  }
+}
