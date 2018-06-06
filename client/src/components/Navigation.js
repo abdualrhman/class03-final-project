0755622dffc5e62132d0b9efd71d20853f97f704
Abdulrhman Ramadan <abdalrhman07094h@gmail.com>
2018-06-06 10:57:48 +0200

    changed the navbar colot

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -9,7 +9,8 @@ import '../styles/index.css'
      const newPath = window.location.pathname.substr(1);
      super(props)
      this.state={
-       class: newPath
+       class: newPath,
+       style : newPath
      }
      this.clickHandler=this.clickHandler.bind(this)
    }
@@ -22,18 +23,14 @@ import '../styles/index.css'
   render(){
     return(
         <div>
-        <nav className="navbar navbar-expand-lg brand">
-        <a className="navbar-brand" href="bla">Navbar</a>
-        <ul className='' >
-        <div className='nav-link-container'>
-        <a className={this.state.class=== 'home' ? 'active nav-item' : 'nav-item'}  onClick={this.clickHandler} name='home'>home</a>
+        <nav className="navbar  brand navbar-default">
+        <ul className='nav-list' >
+        <a className={this.state.class=== 'home' ? 'active nav-item' : 'nav-item'} onClick={this.clickHandler} name='home'>home</a>
         <a className={this.state.class=== 'add-contents' ? 'active nav-item' : 'nav-item'}  onClick={this.clickHandler} name='add-contents'>add contents</a>
         <a className={this.state.class=== 'categories' ? 'active nav-item' : 'nav-item'}  onClick={this.clickHandler} name='categories'>categories</a>
         <a className={this.state.class=== 'about' ? 'active  nav-item' : 'nav-item'}  onClick={this.clickHandler} name='about'>about</a>
-        </div>
         </ul>
         </nav>
-        {console.log(window.location.pathname )}
         </div>
     );
   }
